3025383b7986b010daf5cec13797cf146608128a
JulienH77 <84439263+JulienH77@users.noreply.github.com>
2026-03-08 16:16:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const cacheName = 'carto-v1';
+// On change la version ici à chaque grosse mise à jour !
+const cacheName = 'carto-v2'; 
 const assets = [
   './',
   './index.html',
@@ -7,7 +8,7 @@ const assets = [
   './bancs.geojson'
 ];
 
-// Installation du Service Worker et mise en cache des fichiers
+// 1. Installation du Service Worker et mise en cache
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
@@ -16,7 +17,19 @@ self.addEventListener('install', e => {
   );
 });
 
-// Récupération des ressources depuis le cache si hors-ligne
+// 2. NOUVEAU : Activation et nettoyage des vieux caches
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key)) // Supprime la v1
+      );
+    })
+  );
+});
+
+// 3. Récupération des ressources depuis le cache si hors-ligne
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => {
